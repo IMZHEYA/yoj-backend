a20136c3f690cdbc1f4b8843588a233bf5066fd6
Serendipityzhezi <1342558165@qq.com>
2024-05-22 19:42:18 +0800

    feat:添加题目VO类

---
+++ src/main/java/com/yupi/yoj/model/vo/QuestionVO.java
@@ -0,0 +1,88 @@
+package com.yupi.yoj.model.vo;
+
+import cn.hutool.json.JSONUtil;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.yupi.yoj.model.dto.question.JudgeConfig;
+import com.yupi.yoj.model.entity.Post;
+import lombok.Data;
+import org.springframework.beans.BeanUtils;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * 题目视图
+ *
+ *
+ *
+ */
+@Data
+public class QuestionVO implements Serializable {
+    /**
+     * id
+     */
+    private Long id;
+
+    /**
+     * 标题
+     */
+    private String title;
+
+    /**
+     * 内容
+     */
+    private String content;
+
+    /**
+     * 标签列表（json 数组）
+     */
+    private List<String> tags;
+
+    /**
+     * 题目提交数
+     */
+    private Integer submitNum;
+
+    /**
+     * 题目通过数
+     */
+    private Integer acceptedNum;
+
+
+
+    /**
+     * 判题配置（json 对象）
+     */
+    private JudgeConfig judgeConfig;
+
+    /**
+     * 点赞数
+     */
+    private Integer thumbNum;
+
+    /**
+     * 收藏数
+     */
+    private Integer favourNum;
+
+    /**
+     * 创建用户 id
+     */
+    private Long userId;
+
+    /**
+     * 创建时间
+     */
+    private Date createTime;
+
+    /**
+     * 更新时间
+     */
+    private Date updateTime;
+
+
+    private static final long serialVersionUID = 1L;
+}
