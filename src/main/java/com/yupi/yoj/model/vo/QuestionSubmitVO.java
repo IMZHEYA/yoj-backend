daca4854245006400a0eb20ac0cc5e0bbc602781
Serendipityzhezi <1342558165@qq.com>
2024-05-23 20:42:51 +0800

    feat:😀添加对题目提交记录的VO包装类

---
+++ src/main/java/com/yupi/yoj/model/vo/QuestionSubmitVO.java
@@ -0,0 +1,116 @@
+package com.yupi.yoj.model.vo;
+
+import cn.hutool.json.JSONUtil;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.yupi.yoj.model.dto.question.JudgeConfig;
+import com.yupi.yoj.model.dto.questionSubmit.JudgeInfo;
+import com.yupi.yoj.model.entity.Question;
+import com.yupi.yoj.model.entity.QuestionSubmit;
+import lombok.Data;
+import org.springframework.beans.BeanUtils;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * 题目视图
+ */
+@Data
+public class QuestionSubmitVO implements Serializable {
+    /**
+     * id
+     */
+    private Long id;
+
+    /**
+     * 编程语言
+     */
+    private String language;
+
+    /**
+     * 用户代码
+     */
+    private String code;
+
+    /**
+     * 判题信息（json 对象）
+     */
+    private JudgeInfo judgeInfo;
+
+    /**
+     * 判题状态（0 - 待判题、1 - 判题中、2 - 成功、3 - 失败）
+     */
+    private Integer status;
+
+    /**
+     * 题目 id
+     */
+    private Long questionId;
+
+    /**
+     * 创建用户 id
+     */
+    private Long userId;
+
+    /**
+     * 创建时间
+     */
+    private Date createTime;
+
+    /**
+     * 更新时间
+     */
+    private Date updateTime;
+
+    /**
+     * 提交用户信息
+     */
+    private UserVO userVO;
+
+
+    /**
+     * 对应题目信息
+     */
+    private QuestionVO questionVO;
+
+    /**
+     * 包装类转对象,属性JAVA对象转为字符串
+     *
+     * @param questionSubmitVO
+     * @return
+     */
+    public static QuestionSubmit voToObj(QuestionSubmitVO questionSubmitVO) {
+        if (questionSubmitVO == null) {
+            return null;
+        }
+        QuestionSubmit questionSubmit = new QuestionSubmit();
+        BeanUtils.copyProperties(questionSubmitVO, questionSubmit);
+        JudgeInfo voJudgeInfo = questionSubmitVO.getJudgeInfo();
+        if(voJudgeInfo != null){
+            String jsonStr = JSONUtil.toJsonStr(voJudgeInfo);
+            questionSubmit.setJudgeInfo(jsonStr);
+        }
+        return questionSubmit;
+    }
+
+    /**
+     * 对象转包装类,把json字符串转为java对象
+     *
+     * @param questionSubmit
+     * @return
+     */
+    public static QuestionSubmitVO objToVo(QuestionSubmit questionSubmit) {
+        if (questionSubmit == null) {
+            return null;
+        }
+        QuestionSubmitVO questionVO = new QuestionSubmitVO();
+        BeanUtils.copyProperties(questionSubmit, questionVO);
+        String info = questionSubmit.getJudgeInfo();
+        questionVO.setJudgeInfo(JSONUtil.toBean(info,JudgeInfo.class));
+        return questionVO;
+    }
+
+    private static final long serialVersionUID = 1L;
+}
