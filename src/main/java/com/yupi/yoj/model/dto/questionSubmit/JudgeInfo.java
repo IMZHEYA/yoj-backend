58906159d936afeb4a8ded8c0a6c6f3f6a844063
Serendipityzhezi <1342558165@qq.com>
2024-05-23 11:55:23 +0800

    feat:😀添加判题信息实体类

---
+++ src/main/java/com/yupi/yoj/model/dto/questionSubmit/JudgeInfo.java
@@ -0,0 +1,16 @@
+package com.yupi.yoj.model.dto.questionSubmit;
+
+import lombok.Data;
+
+@Data
+public class JudgeInfo {
+
+    private String message;
+
+
+    private Long time;
+
+
+
+    private Long memory;
+}
