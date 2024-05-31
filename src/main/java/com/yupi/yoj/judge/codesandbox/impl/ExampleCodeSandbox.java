0357dedfa5e604a5994dbef4f27c9867e801e2f4
Serendipityzhezi <1342558165@qq.com>
2024-05-31 21:18:28 +0800

    feat：😉实现示例的代码沙箱

--- src/main/java/com/yupi/yoj/judge/codesandbox/impl/ExampleCodeSandbox.java
+++ src/main/java/com/yupi/yoj/judge/codesandbox/impl/ExampleCodeSandbox.java
@@ -3,6 +3,11 @@ package com.yupi.yoj.judge.codesandbox.impl;
 import com.yupi.yoj.judge.codesandbox.CodeSandbox;
 import com.yupi.yoj.judge.codesandbox.model.ExecutecodeCodeRequest;
 import com.yupi.yoj.judge.codesandbox.model.ExecutecodeResponse;
+import com.yupi.yoj.model.dto.questionSubmit.JudgeInfo;
+import com.yupi.yoj.model.enums.JudgeInfoMessageEnum;
+import com.yupi.yoj.model.enums.QuestionSubmitStatusEnum;
+
+import java.util.List;
 
 /**
  * 示例代码沙箱
@@ -10,7 +15,19 @@ import com.yupi.yoj.judge.codesandbox.model.ExecutecodeResponse;
 public class ExampleCodeSandbox implements CodeSandbox {
     @Override
     public ExecutecodeResponse executeCode(ExecutecodeCodeRequest excodeCodeRequest) {
-        System.out.println("示例代码沙箱");
-        return null;
+
+        List<String> inputList = excodeCodeRequest.getInputList();
+        String code = excodeCodeRequest.getCode();
+        String language = excodeCodeRequest.getLanguage();
+        ExecutecodeResponse executecodeResponse = new ExecutecodeResponse();
+        executecodeResponse.setOutputList(inputList);
+        executecodeResponse.setMessage("测试执行成功");
+        executecodeResponse.setStatus(QuestionSubmitStatusEnum.SUCCEED.getValue());
+        JudgeInfo judgeInfo = new JudgeInfo();
+        judgeInfo.setMessage(JudgeInfoMessageEnum.ACCEPTED.getText());
+        judgeInfo.setTime(100L);
+        judgeInfo.setMemory(100L);
+        executecodeResponse.setJudgeInfo(judgeInfo);
+        return executecodeResponse;
     }
 }
--- src/main/java/com/yupi/yoj/model/dto/questionSubmit/JudgeInfo.java
+++ src/main/java/com/yupi/yoj/model/dto/questionSubmit/JudgeInfo.java
@@ -5,12 +5,18 @@ import lombok.Data;
 @Data
 public class JudgeInfo {
 
+    /**
+     * 程序执行信息
+     */
     private String message;
 
-
+    /**
+     * 程序执行消耗时间
+     */
     private Long time;
 
-
-
+    /**
+     * 程序执行消耗内存
+     */
     private Long memory;
 }
