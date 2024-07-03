a1ffcf51205cd78c3ac9e230b92c95c08dd5eda8
Serendipityzhezi <1342558165@qq.com>
2024-07-03 15:09:05 +0800

    feat:😃完善实现调用远程代码沙箱

--- src/main/java/com/yupi/yoj/judge/codesandbox/impl/RemoteCodeSandbox.java
+++ src/main/java/com/yupi/yoj/judge/codesandbox/impl/RemoteCodeSandbox.java
@@ -1,5 +1,10 @@
 package com.yupi.yoj.judge.codesandbox.impl;
 
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.http.HttpUtil;
+import cn.hutool.json.JSONUtil;
+import com.yupi.yoj.common.ErrorCode;
+import com.yupi.yoj.exception.BusinessException;
 import com.yupi.yoj.judge.codesandbox.CodeSandbox;
 import com.yupi.yoj.judge.codesandbox.model.ExecutecodeCodeRequest;
 import com.yupi.yoj.judge.codesandbox.model.ExecutecodeResponse;
@@ -10,7 +15,15 @@ import com.yupi.yoj.judge.codesandbox.model.ExecutecodeResponse;
 public class RemoteCodeSandbox implements CodeSandbox {
     @Override
     public ExecutecodeResponse executeCode(ExecutecodeCodeRequest excodeCodeRequest) {
-        System.out.println("远程代码沙箱");
-        return null;
+        String url = "http://localhost:8090/executeCode";
+        String jsonStr = JSONUtil.toJsonStr(excodeCodeRequest);
+        String responseStr = HttpUtil.createPost(url)
+                .body(jsonStr)
+                .execute()
+                .body();
+        if(StrUtil.isBlank(responseStr)){
+            throw new BusinessException(ErrorCode.API_REQUEST_ERROR,"调用远程代码沙箱接口失败：" + responseStr);
+        }
+        return JSONUtil.toBean(responseStr, ExecutecodeResponse.class);
     }
 }
